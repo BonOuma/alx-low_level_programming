482e2c8795e7ba174b47fb353e07feb446fc7737
BonOuma <mcouma.mugah@gmail.com>
2023-02-18 00:34:32 +0300

    correction

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,18 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
-
 /**
  * main - Entry point
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-        char lowerCase;
+	char low;
 
-        for (lowerCase = 'a'; lowerCase <= 'z'; lowerCase++)
-        {
-                putchar(lowerCase);
-        }
-        putchar('\n');
-        return (0);
+	for (low = 'a'; low <= 'z'; low++)
+		putchar(low);
+		printf("\n");
+
+	return (0);
 }
--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,20 +1,20 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
-
 /**
  * main - Entry point
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-        char letters[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        int i;
+	char low;
 
-        for (i = 0; i <= 51; i++)
-        {
-                putchar(letters[i]);
-        }
-        putchar('\n');
-        return (0);
+	for (low = 'a'; low <= 'z'; low++)
+		putchar(low);
+	for (low = 'A'; low <= 'Z'; low++)
+		putchar(low);
+		putchar('\n');
+
+	return (0);
 }
